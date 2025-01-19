50dbe48366736cbc783b64ff58c9f5c778b06051
anzalmohamed <anzaldidah@gmail.com>
2025-01-19 15:39:16 +0300

    worked on new quote and tweet button

--- script.js
+++ script.js
@@ -10,5 +10,8 @@ async function getquote(url) {
 }
 getquote(api_url);
 
+function tweet(){
+    window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "----by " + author.innerHTML, "Tweet Window", "width=600, height=300");
+}
 
 
